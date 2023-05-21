28558a00b65f5e83a504f30d9c1a262bf7d0e44d
Arturo12321 <valerio.manrique@tecsup.edu.pe>
2023-05-20 23:03:45 -0500

    Agregar archivos de proyecto.

---
+++ Demo092023A/Demo092023A/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Demo092023A
+{
+    public partial class MainPage : ContentPage
+    {
+     
+        public MainPage()
+        {
+            InitializeComponent();
+
+            /*
+           Paso 1: Declarar una instancia del gesture a utilizar
+           Paso 2: Creo un Evento
+           Paso 3: Asigno mi evento creado en el paso2
+           Paso 4: Asignarr el gesto a  un control.
+           */
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Tap());
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Pinch());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Swipe());
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new PinchDemo());
+            };
+        } 
+
+    }
+}
---
+++ Demo092023A/Demo092023A/Pinch.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo092023A
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pinch : ContentPage
+    {
+        bool isVisible = false;
+        public Pinch()
+        {
+            InitializeComponent();
+
+            var pinch1 = new PinchGestureRecognizer();
+            pinch1.PinchUpdated += EventPinch;
+            image1.GestureRecognizers.Add(pinch1);
+        }
+        void EventPinch(object sender, EventArgs e)
+        {
+            isVisible = !isVisible;
+
+            if (isVisible)
+                lblPinch.BackgroundColor = Color.Black;
+            else
+                lblPinch.BackgroundColor = Color.Red;
+        }
+
+
+    }
+}
